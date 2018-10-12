433734124d45c9547d5bf5db41c59d687e82795e
BT_1N3_32 <kwu8742@bths.edu>
2018-10-12 08:39:47 -0400

    Guesser

--- src/KarenWu/Guesser.java
+++ src/KarenWu/Guesser.java
@@ -1,8 +1,13 @@
-//package KarenWu;
+package KarenWu;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
-//public class Guesser {
+public class Guesser {
 
-    //}
-//}
+    public static String guesser;
+    Scanner input = new Scanner(System.in);
+    int guess = 0;
+    System.out.print("Guess the number.");
+    int num = input.nextInt();
+    }
+}
