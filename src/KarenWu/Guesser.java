f59dbddfbf4e604af2780260c9229b5707e2fe97
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2018-11-14 13:47:49 -0500

    Update Guesser.java

--- src/KarenWu/Guesser.java
+++ src/KarenWu/Guesser.java
@@ -7,7 +7,20 @@ public class Guesser {
     public static String guesser;
     Scanner input = new Scanner(System.in);
     int guess = 0;
-    System.out.print("Guess the number.");
+    int guessnum = 50;
+    System.out.print("Input number to be Guessed between 1-100.");
     int num = input.nextInt();
+    while(guessnum != num) {
+        System.out.println("Is your number" + guessnum +"?");
+        System.out.println("Is it higher or lower?");
+        String updown = input.nextString();
+        if(updown == "higher") {
+            guessnum = guessnum + guessnum/2;
+        }
+        else {
+            guessnum = guessnum/2;
+        }
+        guess++;
     }
+    System.out.println("Your number is " + guessnum ". I took " + guess "tries.");
 }
