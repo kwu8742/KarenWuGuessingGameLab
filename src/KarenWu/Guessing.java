96a88bd26b131973ca481d17375d870661de7a82
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2018-11-15 14:34:37 -0500

    Update Guessing.java

--- src/KarenWu/Guessing.java
+++ src/KarenWu/Guessing.java
@@ -7,7 +7,9 @@ public class Guessing {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num = (int)(1 + Math.random() * 10);
-        System.out.print("Guess the number.");
+        System.out.print("What is your name?");
+        String name = input.nextline();
+        System.out.println("Guess the number " + name ".");
         int score = 0;
         int guess;
         guess = input.nextInt();
@@ -17,6 +19,7 @@ public class Guessing {
             score = score + 1;
             guess = input.nextInt();
         }
+        System.out.println("Congrats " + name "!");
         System.out.println("You are correct, you guessed " + score + " times");
         }
     }
