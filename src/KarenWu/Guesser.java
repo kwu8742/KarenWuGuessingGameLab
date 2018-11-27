7c6a0bf635abbac5affa0933ba0680be2db5ff7d
BT_1N3_34 <kwu8742@bths.edu>
2018-11-27 08:38:14 -0500

    Final

--- src/KarenWu/Guesser.java
+++ src/KarenWu/Guesser.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class Guesser {
 
-    public static String guesser;
-    Scanner input = new Scanner(System.in);
-    int guess = 0;
-    int guessnum = 50;
-    System.out.print("What is your name?");
-    String name = input.nextLine();
-    System.out.println("Input number to be Guessed between 1-100.");
-    int num = input.nextInt();
-    while(guessnum != num) {
-        System.out.println("Is your number" + guessnum + name + "?");
-        System.out.println("Is it higher or lower " + name "?");
-        String updown = input.nextLine();
-        if(updown == "higher") {
-            guessnum = guessnum + guessnum/2;
+    public static void guesser(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int guess = 0;
+        int guessnum = 50;
+        System.out.print("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Input number to be Guessed between 1-100.");
+        int num = input.nextInt();
+        while (guessnum != num) {
+            System.out.println("Is your number" + guessnum + name + "?");
+            System.out.println("Is it higher or lower " + name + "?");
+            String updown = input.nextLine();
+            if (updown == "higher") {
+                guessnum = guessnum + guessnum / 2;
+            } else {
+                guessnum = guessnum / 2;
+            }
+            guess++;
         }
-        else {
-            guessnum = guessnum/2;
-        }
-        guess++;
+        System.out.println("Your number is " + guessnum + ". I took " + guess + "tries.");
     }
-    System.out.println("Your number is " + guessnum ". I took " + guess "tries.");
 }
--- src/KarenWu/Guessing.java
+++ src/KarenWu/Guessing.java
@@ -9,17 +9,16 @@ public class Guessing {
         int num = (int)(1 + Math.random() * 10);
         System.out.print("What is your name?");
         String name = input.nextLine();
-        System.out.println("Guess the number " + name ".");
+        System.out.println("Guess the number " + name + ".");
         int score = 0;
         int guess;
         guess = input.nextInt();
-        while (guess != num)
-    {
+        while (guess != num) {
             System.out.print("Guess again:");
             score = score + 1;
             guess = input.nextInt();
         }
-        System.out.println("Congrats " + name "!");
+        System.out.println("Congrats " + name + "!");
         System.out.println("You are correct, you guessed " + score + " times");
-        }
     }
+}
